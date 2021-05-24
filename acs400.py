502bf398c7f205ba2ce7fcd445678382b9ead2c0
Daniel Gullberg <daniel_gullberg@hotmail.com>
2021-05-24 17:45:23 +0200

    Add template

--- acs400.py
+++ acs400.py
@@ -10,15 +10,18 @@ class ACS400:
 
     def printNPump(self):
         nPump=self.getNPump()
-        print(f"{nPump}")
-
+        if not nPump.isError():
+            print(f"{nPump.registers[0]}")
+        else:
+            print(f"{nPump}")
+            
     def getNPump(self):
         nPump = self.client.read_holding_registers(102-1,count=1,unit=1)
-        return nPump.registers[0]
+        return nPump
 
 
 if __name__=="__main__":
-    acs400=ACS400(port="/dev/ttyUSB1")
+    acs400=ACS400(port="/dev/ttyUSB0")
     for i in range(5):
         acs400.printNPump()
         time.sleep(1)
--- blynk_app.py
+++ blynk_app.py
@@ -11,25 +11,26 @@ config.read("blynk_app.ini")
 BLYNK_AUTH = config['AUTH']['token'].strip('\"')
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
+port = config['ACS400']['port']
+
 # create timers dispatcher instance
 timer = blynktimer.Timer()
 
 WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_WRITE] Pin: V{} Value: '{}'"
 
-fInv = acs400.ACS400(port="/dev/ttyUSB1")
+fInv = acs400.ACS400(port=port)
 
 # Code below: register two timers for different pins with different intervals
 # run_once flag allows to run timers once or periodically
 @timer.register(vpin_num=0, interval=4, run_once=False)
 def write_to_virtual_pin(vpin_num=1):
-    value = fInv.getNPump()
-    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
-    blynk.virtual_write(vpin_num, value)
-
-
-
-print(BLYNK_AUTH)
-    
+    result = fInv.getNPump()
+    if not result.isError():
+        nPump = result.registers[0]
+        print(WRITE_EVENT_PRINT_MSG.format(vpin_num, nPump))
+        blynk.virtual_write(vpin_num, nPump)
+    else:
+        print(f"{result}")
 
 while True:
     blynk.run()
