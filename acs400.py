8bebcd9f83a29c1d777a604f9a9c17cd531c71d9
Daniel Gullberg <daniel_gullberg@hotmail.com>
2021-05-25 19:53:06 +0200

    Add dump method

--- acs400.py
+++ acs400.py
@@ -1,5 +1,9 @@
 from pymodbus.client.sync import ModbusSerialClient
 import time
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
 
 class ACS400:
     def __init__(self,port):
@@ -19,9 +23,37 @@ class ACS400:
         nPump = self.client.read_holding_registers(102-1,count=1,unit=1)
         return nPump
 
+    def getRegister(self, reg):
+        """Get register"""
+        return self.client.read_holding_registers(reg-1,count=1,unit=1)
+
+    def dumpAll(self):
+        """Dump all parameters"""
+        groups = [99,1,10,11,12,13,14,15,16,20]
+        groups += [21,22,25,26,30,31,32,33,34,40,41]
+        groups += [50,51,52,81]
+        for group in groups:
+            regs = [group*100+reg for reg in range(1,100)]
+            for reg in regs:
+                result = self.getRegister(reg)
+                if not result.isError():
+                    logger.info(f"{reg:04} {result.registers[0]:}")
 
 if __name__=="__main__":
-    acs400=ACS400(port="/dev/ttyUSB0")
-    for i in range(5):
-        acs400.printNPump()
-        time.sleep(1)
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)-7s][%(name)s] %(message)s")
+    rootLogger = logging.getLogger()
+     
+    fileHandler = logging.FileHandler("acs400.log")
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+     
+    consoleHandler = logging.StreamHandler(sys.stdout)
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+     
+    rootLogger.setLevel(logging.INFO)
+    acs400 = ACS400(port="/dev/ttyUSB1")
+    acs400.dumpAll()
+    # for i in range(5):
+    #     acs400.printNPump()
+    #     time.sleep(1)
